72614edba705b8a8e25c74c08094b3073042e8c2
Patrick Bartlett <moopbop@gmail.com>
2015-05-06 12:40:35 -0400

    Attempt at moving platforms

---
+++ Assets/Scripts/Base/MovingPlatform.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public Transform target;
+	public float speed;
+	public Transform start;
+	private bool direction;
+
+	void Start() {
+		direction = false;
+	}
+
+	void Update() {
+		float step = speed * Time.deltaTime;
+
+		if (direction == false) {
+			Vector3 nextPos = Vector3.MoveTowards (this.transform.position, target.position, step);
+
+			if (nextPos != target.position) {
+				this.transform.position = nextPos;
+			}
+			else {
+				direction = true;
+			}
+		}
+
+		if (direction == true) {
+			Vector3 nextPos = Vector3.MoveTowards (this.transform.position, target.position, -step);
+
+			if (nextPos != start.position) {
+				this.transform.position = nextPos;
+			}
+			else {
+				direction = false;
+			}
+		}
+
+		Debug.Log (start.position);
+	}
+}
